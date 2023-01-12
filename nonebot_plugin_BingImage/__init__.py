a5955811a2921750194e39a0b97a0645a9f77ff0
wuchangjun233 <2101546835@qq.com>
2023-01-12 14:22:39 +0800

    🐛 修复部分BUG

--- nonebot_plugin_BingImage/__init__.py
+++ nonebot_plugin_BingImage/__init__.py
@@ -1,28 +1,24 @@
-from nonebot import on_command, on_startswith
+from nonebot import on_command
 from nonebot.typing import T_State
 from nonebot.adapters import Bot, Event
-from nonebot.adapters.ntchat import Message, MessageSegment
+from nonebot.adapters.ntchat import MessageSegment
 from nonebot.adapters.ntchat import MessageEvent
 from nonebot.log import logger
-from nonebot.rule import to_me
-from nonebot import require
 
-require("nonebot_plugin_apscheduler")
-from nonebot_plugin_apscheduler import scheduler
 
 from .utils import *
 
 Help_info = "Bing机器人帮助 \n🌈世界那么大，通过Bing到处看看~ \n🗺 /bing 获取今天的美图(图和故事) \n💻/bing电脑 获取今天的美图(单横板图片)\n📱/bing手机 获取今天的美图(单竖版图片)\n🔧更多功能开发中🔧 "
 
-help_matcher = on_command("bing帮助", aliases={'Bing帮助', '必应帮助', }, rule=to_me(),priority=1)
-bing_matcher = on_command("bing", aliases={'必应', 'bing每日一图', 'bing美图', 'Bing', 'BING'}, rule=to_me(),priority=7)
+help_matcher = on_command("bing帮助", aliases={'Bing帮助', '必应帮助', },priority=21)
+bing_matcher = on_command("bing", aliases={'必应', 'bing每日一图', 'bing美图', 'Bing', 'BING'},priority=22)
 
 bing_desktop_matcher = on_command("bing电脑图片",
                                   aliases={'bing桌面端图片', '电脑图片', '桌面端图片', 'bing电脑', 'bing桌面图片',
-                                           'bing横版'}, rule=to_me(),priority=3)
+                                           'bing横版'}, priority=23)
 bing_mobile_matcher = on_command("bing手机图片",
                                  aliases={'bing移动端图片', '手机图片', '移动端图片', 'bing手机', 'bing竖版', },
-                                 rule=to_me(),priority=4)
+                                 priority=24)
 
 
 @help_matcher.handle()
@@ -36,13 +32,13 @@ async def bing(bot: Bot, event: Event, state: T_State):
     description = getBingDescription()
     msg_img = MessageSegment.image(d_url)
     logger.info("图片获取成功!")
-    msg_title = Message(str(description['title']))
-    msg_headline = Message(str("图片主题：" + description['headline']))
-    msg_desc = Message(str("图片故事：" + description['description']))
-    msg_maintext = Message(str("图片介绍：" + description['main_text']))
-    msg = msg_title + Message("\n") + Message("\n") + msg_headline + Message("\n") + Message(
-        "\n") + msg_maintext + Message("\n") + Message("\n") + msg_desc + Message("\n") + Message("\n")
-    await bing_matcher.send(MessageSegment.text(msg) + MessageSegment.image(msg_img))
+    msg_title = str(description['title'])
+    msg_headline = str("图片主题：" + description['headline'])
+    msg_desc = str("图片故事：" + description['description'])
+    msg_maintext = str("图片介绍：" + description['main_text'])
+    msg = msg_title + ("\n") + ("\n") + msg_headline + ("\n") + (
+        "\n") + msg_maintext + ("\n") + ("\n") + msg_desc
+    await bing_matcher.send(MessageSegment.text(msg) + msg_img)
     logger.info("消息发送成功!")
 
 
